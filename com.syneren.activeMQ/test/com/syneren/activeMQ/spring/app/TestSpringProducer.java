891ba7c1a153dbe5b0257c74be709cab9bf4f865
admin <vkamath@syneren.com>
2013-01-23 14:32:52 -0500

    Corrected for embedded ActiveMQ broker Removed Main Class and instead run the Junit Test case

--- com.syneren.activeMQ/test/com/syneren/activeMQ/spring/app/TestSpringProducer.java
+++ com.syneren.activeMQ/test/com/syneren/activeMQ/spring/app/TestSpringProducer.java
@@ -1,5 +1,29 @@
 package com.syneren.activeMQ.spring.app;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-app.xml")
 public class TestSpringProducer {
+	@Autowired
+	private ConfigurableApplicationContext context;
+
+	@Test
+	public void testMessageFlow() {
+		ISpringProducer producer = (ISpringProducer) context.getBean("springProducer");
+		String text = "THIS IS A STUPID TEXT MESSAGE";
+		producer.springSendTextMessage(text);
+	}
 	
+	@After
+	public void tearDown(){
+		context.close();
+	}
+
 }
